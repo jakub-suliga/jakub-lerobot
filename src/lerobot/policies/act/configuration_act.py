da15eab8e9aeed8f49d938d92d742cdf3bf97b87
jakub-suliga <j.p.suliga@gmail.com>
2026-03-09 11:49:50 +0100

    fix(act): enable pre-norm in ACT configuration for improved model performance

--- src/lerobot/policies/act/configuration_act.py
+++ src/lerobot/policies/act/configuration_act.py
@@ -109,7 +109,7 @@ class ACTConfig(PreTrainedConfig):
     pretrained_backbone_weights: str | None = "ResNet18_Weights.IMAGENET1K_V1"
     replace_final_stride_with_dilation: int = False
     # Transformer layers.
-    pre_norm: bool = False
+    pre_norm: bool = True
     dim_model: int = 512
     n_heads: int = 8
     dim_feedforward: int = 3200
